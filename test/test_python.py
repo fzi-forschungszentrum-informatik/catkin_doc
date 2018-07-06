93ead124c1f4b1bb0a2e4daa12459dadd9a29892
Lena Winter <lwinter@fzi.de>
2018-07-06 10:07:05 +0200

    Added test to check if README.md is created.

--- test/test_python.py
+++ test/test_python.py
@@ -2,6 +2,7 @@
 
 import unittest
 import catkin_doc.python
+import os.path
 
 class TestPython(unittest.TestCase):
     """Test basic functionality of the python doc module"""
@@ -13,9 +14,22 @@ class TestPython(unittest.TestCase):
         self.assertTrue(
             node.extract_params(
                 "self.stop_robot_topic = rospy.get_param('~stop_robot_topic')"))
+        self.assertTrue(
+            node.extract_params(
+                'global_name = rospy.get_param("/global_name")'))
 
     def test_extract_false(self):
         node = catkin_doc.python.PythonParser()
         self.assertFalse(
             node.extract_params( "rospy.loginfo('~stop_robot_topic')"))
+
+    def test_file_exist(self):
+        parser = catkin_doc.python.PythonParser()
+        parser.extract_params(
+          "self.stop_robot_topic = rospy.get_param('~stop_robot_topic', '/hello')")
+        parser.extract_params(
+          "self.stop_robot_topic = rospy.get_param('~start_robot_topic')")
+        parser._node.node_to_md()
+        self.assertTrue(
+          os.path.isfile("README.md"))
 
