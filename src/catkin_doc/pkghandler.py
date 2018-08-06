221df234e560b4a98f270005338140e79f8fc2c3
Lena Winter <lwinter@fzi.de>
2018-08-06 10:57:05 +0200

    Added comments

--- src/catkin_doc/pkghandler.py
+++ src/catkin_doc/pkghandler.py
@@ -6,8 +6,13 @@ class PkgHandler:
     def __init__(self):
         pass
 
+
     @staticmethod
     def search_for_python(pkg_name):
+        """
+        Method which searches through a whole package for python ros nodes
+        Currently prints filenames of files which are probably python ros nodes
+        """
         for filename in os.listdir(pkg_name):
             if os.path.isdir(pkg_name + "/" + filename):
                 PkgHandler.search_for_python(pkg_name + "/" + filename)
@@ -19,6 +24,11 @@ class PkgHandler:
 
     @staticmethod
     def check_if_ros_node(filename):
+        """
+        Method which checks if file contains the string "rospy.init_node"
+        as this is a good clue that this file may be a python ros node.
+        Returns True if stri8ng is containes False otherwise
+        """
         file = open(filename, "r")
         if "rospy.init_node" in file.read():
             return True
