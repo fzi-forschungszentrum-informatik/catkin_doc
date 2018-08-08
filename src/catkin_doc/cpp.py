d2dac7af080f815bee457d90a926a10a2af30cce
Lena Winter <lwinter@fzi.de>
2018-08-08 12:09:24 +0200

    Added parse method.

--- src/catkin_doc/cpp.py
+++ src/catkin_doc/cpp.py
@@ -16,9 +16,19 @@ class CppParser(object):
         """
         Parses every file belonging to the node.
         """
+        #TODO same as in PythonParser concatenating lines is not so nice aslo it is not necessary to parse the header for anything but comments
         for file in self.files:
             with open(filename) as filecontent:
                 self.lines = filecontent.readlines()
+                linenumber = 0
+            while linenumber < len(self.lines) - 2:
+                line = self.lines[linenumber].lstrip(' ').strip('\n') + ' ' +  self.lines[linenumber+1].lstrip(' ').strip('\n') + ' ' + self.lines[linenumber+2].lstrip(' ')
+                for extract,add in self.parser_fcts:
+                    success, key, value = extract(line)
+                    if success:
+                        comment = None
+                        add(key, value, comment)
+                linenumber += 1
 
 
 
