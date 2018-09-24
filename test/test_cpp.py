577cac8d51af3cde4cc2eb21c356187512bb6409
Lena Winter <lwinter@fzi.de>
2018-09-24 10:39:34 +0200

    Changed python test to new function return values as I forgot this some months ago. Curiosly the test didn't fail then.

--- test/test_cpp.py
+++ test/test_cpp.py
@@ -4,20 +4,21 @@ import unittest
 import catkin_doc.cpp
 import os.path
 
-class TestPython(unittest.TestCase):
-    """Test basic functionality of the python doc module"""
+class TestCpp(unittest.TestCase):
+    """Test basic functionality of the cpp doc module"""
+
     def test_extract_params(self):
         #Just add setup.py as file to parse as a file is needed to instance the parser but it is not really used here
         node = catkin_doc.cpp.CppParser("test", ["setup.py"])
         self.assertTrue(
             node.extract_param(
-                'ros::param::param<std::string>("param1", param1, "default_value1");'))
+                'ros::param::param<std::string>("param1", param1, "default_value1");')[0])
+        self.assertFalse(
+            node.extract_param(
+                'ros::param::get("/param2", param2)')[0])
+        self.assertFalse(
+            node.extract_param(
+                'nh.getParam("param3", param3)')[0])
         self.assertTrue(
             node.extract_param(
-                'ros::param::get("/param2", param2)'))
-        self.assertTrue(
-            node.extract_param(
-                'nh.getParam("param3", param3)'))
-        self.assertTrue(
-            node.extract_param(
-                'nh.param<std::string>("param4", param4, "default_value4");'))
+                'nh.param<std::string>("param4", param4, "default_value4");')[0])
--- test/test_python.py
+++ test/test_python.py
@@ -10,90 +10,90 @@ class TestPython(unittest.TestCase):
         node = catkin_doc.python.PythonParser("setup.py")
         self.assertTrue(
             node.extract_param(
-                "self.stop_robot_topic = rospy.get_param('~stop_robot_topic', '/hello')"))
+                "self.stop_robot_topic = rospy.get_param('~stop_robot_topic', '/hello')")[0])
         self.assertTrue(
             node.extract_param(
-                "self.stop_robot_topic = rospy.get_param('~stop_robot_topic')"))
+                "self.stop_robot_topic = rospy.get_param('~stop_robot_topic')")[0])
         self.assertTrue(
             node.extract_param(
-                'global_name = rospy.get_param("/global_name")'))
+                'global_name = rospy.get_param("/global_name")')[0])
 
     def test_extract_params_false(self):
         node = catkin_doc.python.PythonParser("setup.py")
         self.assertFalse(
-            node.extract_param( "rospy.loginfo('~stop_robot_topic')")[1])
+            node.extract_param( "rospy.loginfo('~stop_robot_topic')")[0])
 
     def test_extract_subs(self):
         parser = catkin_doc.python.PythonParser("setup.py")
         self.assertTrue(
             parser.extract_sub(
-                'rospy.Subscriber("chatter", String, callback)'))
+                'rospy.Subscriber("chatter", String, callback)')[0])
         self.assertTrue(
             parser.extract_sub(
-                'self.joint_state_sub = rospy.Subscriber("pathloader/reordered_joint_states", JointState, self.joint_status_changed)'))
+                'self.joint_state_sub = rospy.Subscriber("pathloader/reordered_joint_states", JointState, self.joint_status_changed)')[0])
 
     def test_extract_subs_false(self):
         parser = catkin_doc.python.PythonParser("setup.py")
         self.assertFalse(
-            parser.extract_sub( "rospy.loginfo('~stop_robot_topic')")[1])
+            parser.extract_sub( "rospy.loginfo('~stop_robot_topic')")[0])
 
     def test_extract_pubs(self):
         parser = catkin_doc.python.PythonParser("setup.py")
         self.assertTrue(
             parser.extract_pub(
-                "pub = rospy.Publisher('chatter', String, queue_size=10)"))
+                "pub = rospy.Publisher('chatter', String, queue_size=10)")[0])
         self.assertTrue(
             parser.extract_pub(
-                "pub = rospy.Publisher('chatter', String, queue_size=10, latch=True)"))
+                "pub = rospy.Publisher('chatter', String, queue_size=10, latch=True)")[0])
 
     def test_extract_pubs_false(self):
         parser = catkin_doc.python.PythonParser("setup.py")
         self.assertFalse(
-            parser.extract_pub( "rospy.loginfo('~stop_robot_topic')")[1])
+            parser.extract_pub( "rospy.loginfo('~stop_robot_topic')")[0])
 
     def test_action_clients(self):
         parser = catkin_doc.python.PythonParser("setup.py")
         self.assertTrue(
             parser.extract_action_client(
-                "self.action_client = actionlib.SimpleActionClient('pathloader', PlayTrajectoryAction)"))
+                "self.action_client = actionlib.SimpleActionClient('pathloader', PlayTrajectoryAction)")[0])
 
     def test_action_clients_false(self):
         parser = catkin_doc.python.PythonParser("setup.py")
         self.assertFalse(
-            parser.extract_action_client( "rospy.loginfo('~stop_robot_topic')")[1])
+            parser.extract_action_client( "rospy.loginfo('~stop_robot_topic')")[0])
 
     def test_service_clients(self):
         parser = catkin_doc.python.PythonParser("setup.py")
         self.assertTrue(
             parser.extract_service_client(
-                "append_points = rospy.ServiceProxy('pathloader/appendPoints', ChangePath)"))
+                "append_points = rospy.ServiceProxy('pathloader/appendPoints', ChangePath)")[0])
 
     def test_service_clients_false(self):
         parser = catkin_doc.python.PythonParser("setup.py")
         self.assertFalse(
-            parser.extract_service_client( "rospy.loginfo('~stop_robot_topic')")[1])
+            parser.extract_service_client( "rospy.loginfo('~stop_robot_topic')")[0])
 
     def test_service(self):
         parser = catkin_doc.python.PythonParser("setup.py")
         self.assertTrue(
             parser.extract_service(
-                "s = rospy.Service('add_two_ints', AddTwoInts, handle_add_two_ints)"))
+                "s = rospy.Service('add_two_ints', AddTwoInts, handle_add_two_ints)")[0])
 
     def test_service_false(self):
         parser = catkin_doc.python.PythonParser("setup.py")
         self.assertFalse(
-            parser.extract_service( "rospy.loginfo('~stop_robot_topic')")[1])
+            parser.extract_service( "rospy.loginfo('~stop_robot_topic')")[0])
 
     def test_action(self):
         parser = catkin_doc.python.PythonParser("setup.py")
         self.assertTrue(
             parser.extract_action(
-                "self._as = actionlib.SimpleActionServer(self._action_name, actionlib_tutorials.msg.FibonacciAction, execute_cb=self.execute_cb, auto_start=False)"))
+                "self._as = actionlib.SimpleActionServer(self._action_name, actionlib_tutorials.msg.FibonacciAction, execute_cb=self.execute_cb, auto_start=False)")[0])
 
     def test_action_false(self):
         parser = catkin_doc.python.PythonParser("setup.py")
         self.assertFalse(
-            parser.extract_action( "rospy.loginfo('~stop_robot_topic')")[1])
+            parser.extract_action( "rospy.loginfo('~stop_robot_topic')")[0])
 
     def test_comment(self):
         parser = catkin_doc.python.PythonParser("setup.py")
