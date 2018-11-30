378f1ad3a6a597b81406cb4f4656ba9bdbbf68eb
Lena Winter <lwinter@fzi.de>
2018-11-30 13:34:10 +0100

    Added compare function for publisher

--- src/catkin_doc/nodecomparator.py
+++ src/catkin_doc/nodecomparator.py
@@ -11,6 +11,7 @@ class NodeComparator(object):
         print("Trying to merge old docu und newly generated docu ... \n Default values are marked by <>\n")
         self.compare_params()
         self.compare_subscriber()
+        self.compare_publisher()
 
     def compare_params(self):
         """
@@ -40,7 +41,7 @@ class NodeComparator(object):
                 else:
                     answer = raw_input("There are two different comments for the parameter " + key + "\n Please choose the comment you want by typing <1> or 2. Choices: \n" +
                                        "1. " + new_comment + "\n2. "+ old_comment)
-                    if answer == 2:
+                    if str(answer) == "2":
                         self.merged_node.add_parameter(key, new_default, old_comment)
                     else:
                         self.merged_node.add_parameter(key, new_default, new_comment)
@@ -75,21 +76,54 @@ class NodeComparator(object):
                 else:
                     answer = raw_input("There are two different comments for the subscriber " + key + "\n Please choose the comment you want by typing <1> or 2. Choices: \n" +
                                        "1. " + new_comment + "\n2. "+ old_comment)
-                    if answer == 2:
-                        self.merged_node.add_parameter(key, new_msg, old_comment)
+                    if str(answer) == "2":
+                        self.merged_node.add_subscriber(key, new_msg, old_comment)
                     else:
-                        self.merged_node.add_parameter(key, new_msg, new_comment)
-
-
+                        self.merged_node.add_subscriber(key, new_msg, new_comment)
             else:
                 self.merged_node.add_subscriber(key, new_msg, new_comment)
 
         for key in self.old_node.subscriber:
             if not key in self.new_node.subscriber:
+                old_msg, old_comment = self.old_node.subscriber[key]
                 answer = raw_input("In the docu generated from code the subscriber " + key + " is not defined. \n" "Do you still want to keep the subscriber " +
                                    key + " msg type: " +  old_msg + "\n"+ old_comment + " (<n>/y)")
                 if answer == "y":
-                    self.merged_node.add_parameter(key, old_msg, old_comment)
+                    self.merged_node.add_subscriber(key, old_msg, old_comment)
+
+    def compare_publisher(self):
+        """
+        Function to compare publisher entries of both nodes
+        and add correct entry to the merged node
+        Cases are basically the same as for params
+        """
+        for key in self.new_node.publisher:
+            new_msg, new_comment = self. new_node.publisher[key]
+            if key in self.old_node.publisher:
+                old_msg, old_comment = self.old_node.publisher[key]
+                if not new_msg == old_msg:
+                    print("Warning! The msg type for the publisher to topic " + key + " has changed.\n")
+                if "Please add description" in old_comment or old_comment == new_comment:
+                    self.merged_node.add_publisher(key, new_msg, new_comment)
+                elif "Please add description" in new_comment:
+                    self.merged_node.add_publisher(key, new_msg, old_comment)
+                else:
+                    answer = raw_input("There are two different comments for the publisher " + key + "\n Please choose the comment you want by typing <1> or 2. Choices: \n" +
+                                       "1. " + new_comment + "\n2. "+ old_comment)
+                    if str(answer) == "2":
+                        self.merged_node.add_publisher(key, new_msg, old_comment)
+                    else:
+                        self.merged_node.add_publisher(key, new_msg, new_comment)
+            else:
+                self.merged_node.add_publisher(key, new_msg, new_comment)
+
+        for key in self.old_node.publisher:
+            if not key in self.new_node.publisher:
+                old_msg, old_comment = self.old_node.publisher[key]
+                answer = raw_input("In the docu generated from code the publisher " + key + " is not defined. \n" "Do you still want to keep the publisher " +
+                                   key + " msg type: " +  old_msg + "\n"+ old_comment + " (<n>/y)")
+                if answer == "y":
+                    self.merged_node.add_publisher(key, old_msg, old_comment)
 
 
 
