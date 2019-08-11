d1a81ce7656d4fd906ebedd7ed63ce8cebc21c44
Felix Mauch <mauch@fzi.de>
2019-08-11 17:43:42 +0200

    adapted service and action url to commit ac716bd

--- src/catkin_doc/datastructures/action.py
+++ src/catkin_doc/datastructures/action.py
@@ -37,11 +37,11 @@ class Action(Topic):
     def __init__(self, name, description="", datatype=""):
         super(Action, self).__init__(name, description, datatype)
         self.type_doc_url_base = "http://docs.ros.org/api/{}/html/action/{}.html"
-        self.url = self.create_url(datatype)
+        self.create_url(datatype)
 
 class ActionClient(Topic):
     """Datastructure representing a Action client"""
     def __init__(self, name, description="", datatype=""):
         super(ActionClient, self).__init__(name, description, datatype)
         self.type_doc_url_base = "http://docs.ros.org/api/{}/html/action/{}.html"
-        self.url = self.create_url(datatype)
+        self.create_url(datatype)
--- src/catkin_doc/datastructures/service.py
+++ src/catkin_doc/datastructures/service.py
@@ -37,7 +37,7 @@ class Service(Topic):
     def __init__(self, name, description="", datatype=""):
         super(Service, self).__init__(name, description, datatype)
         self.type_doc_url_base = "http://docs.ros.org/api/{}/html/srv/{}.html"
-        self.url = self.create_url(datatype)
+        self.create_url(datatype)
 
 
 class ServiceClient(Topic):
@@ -45,4 +45,4 @@ class ServiceClient(Topic):
     def __init__(self, name, description="", datatype=""):
         super(ServiceClient, self).__init__(name, description, datatype)
         self.type_doc_url_base = "http://docs.ros.org/api/{}/html/srv/{}.html"
-        self.url = self.create_url(datatype)
+        self.create_url(datatype)
